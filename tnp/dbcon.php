5fa7cc17d6544fcb1deae5f321a4762978d0a63f
Niraj Kumar Das <niraj_kr_das@yahoo.in>
2015-03-22 13:50:07 +0530

    Training and Placement Cell Management System
    Training and Placement Cell Management System

--- tnp/dbcon.php
+++ tnp/dbcon.php
@@ -2,7 +2,7 @@
 	$log = $_POST;
 	$dbhost = "localhost";
 	$dbuser = "root";
-	$dbpass = "codexfor9";
+	$dbpass = "";
 	$dbname = "tn";
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 	if(mysqli_connect_errno()){
